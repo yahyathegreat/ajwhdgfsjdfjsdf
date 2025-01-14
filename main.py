f2c1e49230ee2b817b1b2ed26a67ab958b4d5a18
yahyathegreat <yahyamardan1234@gmail.com>
2025-01-14 20:23:54 +0500

    asdfg

--- main.py
+++ main.py
@@ -42,3 +42,14 @@ def int_to_roman(num):
     return roman_numeral
 
 print(int_to_roman(1994))
+class Dog:
+    def __init__(self, breed, age, name):
+        self.breed = breed
+        self.age = age
+        self.name = name
+    
+    def display_info(self):
+        return f"Dog Name: {self.name}, Breed: {self.breed}, Age: {self.age}"
+
+dog = Dog("Golden Retriever", 3, "Buddy")
+print(dog.display_info())
